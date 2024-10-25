35c4b58f974dbf37c24dcfe97ed678c158f9db6b
Bhushan G Y <bhushangy44@gmail.com>
2024-10-25 23:59:31 +0530

    done

--- project-toast/src/components/ToastShelf/ToastShelf.js
+++ project-toast/src/components/ToastShelf/ToastShelf.js
@@ -6,7 +6,19 @@ import { ToastContext } from "../ToastProvider";
 import styles from "./ToastShelf.module.css";
 
 function ToastShelf() {
-  const { toastArray } = React.useContext(ToastContext);
+  const { toastArray, removeAllToasts } = React.useContext(ToastContext);
+
+  React.useEffect(() => {
+    const escKeyPressHandler = (e) => {
+      if (!(e.key === "Escape")) return;
+
+      removeAllToasts();
+    };
+
+    document.addEventListener("keydown", escKeyPressHandler);
+
+    return () => document.removeEventListener("keydown", escKeyPressHandler);
+  }, []);
 
   return (
     <ol className={styles.wrapper}>
